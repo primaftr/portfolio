0854cba3ff08afc9889f52a3ba60f9961a813c33
primeXist <primafitra123@gmail.com>
2023-02-11 15:39:03 +0700

    fix: ChakraNextImage comp missing props

--- src/components/ChakraNextImage.tsx
+++ src/components/ChakraNextImage.tsx
@@ -4,11 +4,13 @@ import Image from "next/image";
 const ChakraNextImage = ({
   src,
   alt,
+  maxW,
+  h,w,
   ...rest
 }: { src: string; alt: string } & Omit<BoxProps, "as">) => {
   return (
-    <Box position="relative" {...rest}>
-      <Image objectFit="cover" layout="fill" src={src} alt={alt} />
+    <Box position="relative" {...rest} h={h} w={w} overflow="hidden">
+      <Image objectFit="cover" src={src} alt={alt} sizes={`max-width:${maxW }`} height={h} width={w} />
     </Box>
   );
 };
