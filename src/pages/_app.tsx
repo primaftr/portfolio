5839ec0c94b3b8f239299380dacd76fea5b8c8a2
primeXist <primafitra123@gmail.com>
2023-02-17 22:38:41 +0700

    fix: gtag manager missing codes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -19,6 +19,8 @@ function MyApp({ Component, pageProps }: AppProps) {
         {`window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
+
+          gtag('config', 'G-JN4MZ9GEHV');
         `}
       </Script>
       <Component {...pageProps} />
