145e83684ce246862f3ac9cc704564555a6ecabd
primeXist <primafitra123@gmail.com>
2023-02-18 18:58:41 +0700

    feat: add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import Head from "next/head";
-
+import { Analytics } from "@vercel/analytics/react";
 import theme from "../theme";
 import { AppProps } from "next/app";
 import Script from "next/script";
@@ -26,6 +26,7 @@ function MyApp({ Component, pageProps }: AppProps) {
           });
         `}
       </Script>
+      <Analytics />
       <Component {...pageProps} />
     </ChakraProvider>
   );
